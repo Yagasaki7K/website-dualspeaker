cdbd69740fee734e1c6b802d1f573e2187efefdc
Anderson Marlon <anderson18.marlon@gmail.com>
2025-05-25 20:01:56 -0300

    Initial commit + PWA + WebRTC - 25-05 20:01

---
+++ src/pages/_app.tsx
@@ -0,0 +1,16 @@
+import { useEffect } from 'react';
+import type { AppProps } from 'next/app';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('/service-worker.js')
+        .then(() => console.log('SW registered'))
+        .catch(err => console.error('SW registration failed:', err));
+    }
+  }, []);
+
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
